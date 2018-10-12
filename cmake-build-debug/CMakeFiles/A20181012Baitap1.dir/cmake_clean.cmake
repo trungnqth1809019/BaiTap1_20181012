bd25dcc33070f4391479a7ee9b7956ffc024fda8
QuangTrung <trungnqth1809019@fpt.edu.vn>
2018-10-12 20:39:09 +0700

    hello

---
+++ cmake-build-debug/CMakeFiles/A20181012Baitap1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/A20181012Baitap1.dir/main.c.obj"
+  "A20181012Baitap1.pdb"
+  "A20181012Baitap1.exe"
+  "A20181012Baitap1.exe.manifest"
+  "libA20181012Baitap1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/A20181012Baitap1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/A20181012Baitap1.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for A20181012Baitap1.
+# This may be replaced when dependencies are built.
